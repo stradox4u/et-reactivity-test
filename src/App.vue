3a81a662f1b9088b36e592b4f53bdd674f6c9c77
Umar <stradox@gmail.com>
2022-07-27 18:58:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import { ref } from 'vue';
+import TestComponent from './components/TestComponent.vue'
+
+export default {
+  components: {TestComponent},
+  setup() {
+    const countProp = ref(0);
+
+    const increment = () => {
+      countProp.value++;
+      console.log(countProp.value);
+    }
+
+    return {
+      countProp,
+      increment
+    }
+  }
+}
+
+</script>
+
+<template>
+  <button @click="increment">Increment</button>
+  <test-component :count="countProp"></test-component>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      required: true
+    }
+  },
+  setup(props, context) {
+    const { count: myCount } = props
+
+    return {
+      myCount
+    }
+  },
+  methods: {
+    incrementLocally() {
+      this.myCount += 1;
+      console.log(this.myCount);
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div>{{ myCount }}</div>
+  <button @click="incrementLocally">Increment Locally</button>
+</template>
